6e6d53900e5c218452ec5d3d7b8daf81577d2e26
tall-rob <robert.streidt@gmail.com>
2021-12-06 20:03:42 -0500

    added unit test coverage and refactored tests

--- projects/ngx-mercury-notifier/src/lib/notifier.component.spec.ts
+++ projects/ngx-mercury-notifier/src/lib/notifier.component.spec.ts
@@ -1,15 +1,17 @@
-import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed, fakeAsync, tick, flush } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 import { MercuryNotifierComponent, MERCURY_NOTIFIER_DEFAULT_SETTINGS } from './notifier.component';
 import { MercuryNotifierModule } from './notifier.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MercuryNotifierSettings } from 'ngx-mercury-notifier';
 
 describe('NotifierComponent', () => {
   let component: MercuryNotifierComponent;
   let fixture: ComponentFixture<MercuryNotifierComponent>;
+  let config: MercuryNotifierSettings;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         MercuryNotifierModule,
@@ -20,17 +22,45 @@ describe('NotifierComponent', () => {
   }));
 
   beforeEach(() => {
+    config = JSON.parse(JSON.stringify(MERCURY_NOTIFIER_DEFAULT_SETTINGS));
     fixture = TestBed.createComponent(MercuryNotifierComponent);
     component = fixture.componentInstance;
-    component.config = JSON.parse(JSON.stringify(MERCURY_NOTIFIER_DEFAULT_SETTINGS));
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  it('Should have assigned host bindings', () => {
+    fixture.detectChanges();
+
+    const checks: Array<boolean> = [];
+
+    checks.push(
+      !!component.leftCSSVar
+    );
+    checks.push(
+      !!component.rightCSSVar
+    );
+    checks.push(
+      !!component.topCSSVar
+    );
+    checks.push(
+      !!component.bottomCSSVar
+    );
+    checks.push(
+      typeof component.bottomUp == 'boolean'
+    );
+
+    expect(
+      checks.every(Boolean)
+    ).toBeTruthy();
+  });
+
   it('should add and remove 1 notification', fakeAsync(() => {
+
+    component.config = config;
+    fixture.detectChanges();
 
     const checks: Array<boolean> = [];
 
@@ -76,9 +106,159 @@ describe('NotifierComponent', () => {
     expect(
       checks.every(Boolean)
     ).toBeTruthy();
+
+    flush();
+  }));
+
+  it('should add, hold, and remove 1 notification', fakeAsync(() => {
+
+    config.rolloverPause = true;
+    component.config = config;
+    fixture.detectChanges();
+
+    const checks: Array<boolean> = [];
+
+    // Should have no notifications
+    checks.push(
+      component.notifications.size === 0
+    );
+
+    // Create 1 notification
+    const message = 'Test!';
+    component.notify(message, null);
+
+    // Should have 1 notification added
+    checks.push(
+      component.notifications.size === 1
+    );
+
+    const [_, notification] = [...component.notifications][0];
+
+    tick(2900);
+
+    component.holdOn(notification);
+
+    tick(9000);
+
+    // Should still have 1 notification added
+    checks.push(
+      component.notifications.size === 1
+    );
+
+    component.holdOff();
+
+    tick(3000);
+
+    // Should have no notifications
+    checks.push(
+      component.notifications.size === 0
+    );
+
+    expect(
+      checks.every(Boolean)
+    ).toBeTruthy();
+
+    flush();
+  }));
+
+  it('should toggle dark/light mode using config theme', fakeAsync(() => {
+
+    component.config = config;
+    fixture.detectChanges();
+
+    const checks: Array<boolean> = [];
+
+    component.config.theme = 'LIGHT-ON-DARK';
+
+    tick(1000);
+
+    fixture.detectChanges();
+
+    // Should have theme class
+    checks.push(
+      fixture.nativeElement.classList.contains('light-on-dark') === true
+    );
+
+    component.config.theme = 'DARK-ON-LIGHT';
+
+    tick(1000);
+
+    fixture.detectChanges();
+
+    // Should have NOT have theme class
+    checks.push(
+      fixture.nativeElement.classList.contains('dark-on-light') === false
+    );
+
+    expect(
+      checks.every(Boolean)
+    ).toBeTruthy();
+
+    flush();
+  }));
+
+  it('should display close button based on canDismiss flag', fakeAsync(() => {
+
+    component.config = config;
+    fixture.detectChanges();
+
+    const checks: Array<boolean> = [];
+
+    // Should have no notifications
+    checks.push(
+      component.notifications.size === 0
+    );
+
+    fixture.detectChanges();
+
+    // canDismiss Should be true
+    checks.push(
+      component.config.canDismiss === true
+    );
+
+    // Create 1 notification that can dismiss based on config
+    const message = 'Test!';
+    component.notify(message, null, 3000);
+
+    tick(1000);
+
+    fixture.detectChanges();
+
+    // Should have close button
+    checks.push(
+      fixture.debugElement.queryAll(By.css('.notifier .close')).length === 1
+    );
+
+    tick(3000);
+
+    // Should have no notifications
+    checks.push(
+      component.notifications.size === 0
+    );
+
+    // Create 1 notification that cannot dismiss
+    component.notify(message, null, 3000, false);
+
+    tick(1000);
+
+    fixture.detectChanges();
+
+    // Should have close button
+    checks.push(
+      fixture.debugElement.queryAll(By.css('.notifier .close')).length === 0
+    );
+
+    expect(
+      checks.every(Boolean)
+    ).toBeTruthy();
+
+    flush();
   }));
 
   it('should merge a duplicate notification', fakeAsync(() => {
+
+    component.config = config;
+    fixture.detectChanges();
 
     const checks: Array<boolean> = [];
 
@@ -115,14 +295,17 @@ describe('NotifierComponent', () => {
       fixture.debugElement.queryAll(By.css('.notifier .tally'))[0].nativeElement.innerHTML === '2'
     );
 
-    tick(Infinity);
-
     expect(
       checks.every(Boolean)
     ).toBeTruthy();
+
+    flush();
   }));
 
   it('should not merge a duplicate notification', fakeAsync(() => {
+
+    component.config = config;
+    fixture.detectChanges();
 
     const checks: Array<boolean> = [];
 
@@ -159,11 +342,11 @@ describe('NotifierComponent', () => {
       fixture.debugElement.queryAll(By.css('.notifier .tally')).length === 0
     );
 
-    tick(Infinity);
-
     expect(
       checks.every(Boolean)
     ).toBeTruthy();
+
+    flush();
   }));
 
 });
--- projects/ngx-mercury-notifier/src/lib/notifier.service.spec.ts
+++ projects/ngx-mercury-notifier/src/lib/notifier.service.spec.ts
@@ -1,30 +1,62 @@
-import { async, TestBed } from '@angular/core/testing';
+import { waitForAsync, TestBed } from '@angular/core/testing';
 
 import { MercuryNotifierService } from './notifier.service';
 import { ComponentRef } from '@angular/core';
-import { MercuryNotifierComponent } from './notifier.component';
+import { MercuryNotifierComponent, MercuryNotifierSettings } from './notifier.component';
 import { MercuryNotifierModule } from './notifier.module';
 import { OverlayRef } from '@angular/cdk/overlay';
 
 describe('MercuryNotifierService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  beforeEach(async(() => {
+  let service: MercuryNotifierService;
+  let settings: MercuryNotifierSettings;
+
+  beforeEach(waitForAsync(() => {
+    settings = { duration: 5000 };
     TestBed.configureTestingModule({
       imports: [
         MercuryNotifierModule,
       ],
+      providers: [
+        {
+          provide: 'MERCURY_NOTIFIER_SETTINGS',
+          useValue: settings
+        },
+      ],
     });
+    service = TestBed.inject(MercuryNotifierService);
   }));
 
+  afterEach(() => service.ngOnDestroy());
+
+  TestBed.configureTestingModule({
+    imports: [
+      MercuryNotifierModule,
+    ],
+  });
+
   it('should be created', () => {
-    const service: MercuryNotifierService = TestBed.inject(MercuryNotifierService);
     expect(service).toBeTruthy();
   });
 
-  it('should attach overlay', () => {
-    const service: MercuryNotifierService = TestBed.inject(MercuryNotifierService);
+  it('should be created with custom setting', () => {
+    expect(service.config.duration === settings.duration).toBeTruthy();
 
+    service.ngOnDestroy();
+  });
+
+  it('should override custom setting', () => {
+
+    const newSettings = { duration: 3000 } as MercuryNotifierSettings;
+    service.applySettings(newSettings);
+
+    expect(service.config.duration === settings.duration).toBeFalsy();
+    expect(service.config.duration === newSettings.duration).toBeTruthy();
+
+    service.ngOnDestroy();
+  });
+
+  it('should attach overlay', () => {
     const overlayRef = (service as any).overlayRef as OverlayRef;
 
     expect(
@@ -32,9 +64,17 @@ describe('MercuryNotifierService', () => {
     ).toBeTruthy();
   });
 
-  it('should create 1 notification', () => {
-    const service: MercuryNotifierService = TestBed.inject(MercuryNotifierService);
+  it('should detach overlay on Destroy', () => {
+    const overlayRef = (service as any).overlayRef as OverlayRef;
 
+    service.ngOnDestroy();
+
+    expect(
+      overlayRef.hasAttached(),
+    ).toBeFalsy();
+  });
+
+  it('should create 1 notification', () => {
     const compRef = (service as any).compRef as ComponentRef<MercuryNotifierComponent>;
 
     service.notify('Test!');
